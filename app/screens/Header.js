b09bd69b95dd09b1f66850d5c33219b19235be34
Suneel Kumar Khatri <khatrisuneel96@gmail.com>
2020-09-05 17:13:13 +0500

    Update Header.js

--- app/screens/Header.js
+++ app/screens/Header.js
@@ -3,10 +3,22 @@ import { StyleSheet, Text, View } from "react-native";
 
 export default function Header() {
   return (
-    <View>
-      <Text>Checking</Text>
+    <View style={styles.header}>
+      <Text style={styles.title}>My Todos</Text>
     </View>
   );
 }
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  header: {
+    height: 80,
+    paddingTop: 40,
+    backgroundColor: "coral",
+  },
+  title: {
+    textAlign: "center",
+    color: "#fff",
+    fontSize: 20,
+    fontWeight: "bold",
+  },
+});
